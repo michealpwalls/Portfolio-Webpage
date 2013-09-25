6069f10a4b0d7b2d98ad8a4abbeb4b53df7b6c5b
Micheal Walls <michealpwalls@gmail.com>
2013-09-24 23:48:25 -0400

    Initial PHP structure and external documentation
    Initial PHP-based content management system started, external
    documentation section with initial Wireframe.

---
+++ html_head.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ *      head.php
+ *      
+ *      Copyright 2013 Micheal Walls <michealpwalls@gmail.com>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ *      
+ *      
+ */
+
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<title><?=$pageTitle;?></title>
+	<meta charset="UTF-8">
+	<meta name="editor" content="Geany 0.20">
+</head>
+<body>
